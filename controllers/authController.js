e91c4b359497e4b08b3c9e5d21622b0a64c4cd4f
mohamdabidi2 <mohamedabidi58@gmail.com>
2025-03-25 13:42:48 +0100

    v2 correct the send of user data

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,7 +20,7 @@ exports.login = async (req, res) => {
   if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
 
   const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1d" });
-  res.json({ token });
+  res.json({ token,user });
 };
 
 exports.getMe = async (req, res) => {
